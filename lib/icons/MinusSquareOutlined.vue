146bf272143edce397870f1b2608ddd324e6476f
AlphaFoxz <841958335@qq.com>
2024-08-09 15:07:44 +0800

    opt: 减少依赖

---
+++ lib/icons/MinusSquareOutlined.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    focusable="false"
+    data-icon="minus-square"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    aria-hidden="true"
+    viewBox="64 64 896 896"
+  >
+    <path d="M328 544h368c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8H328c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8z"></path>
+    <path
+      d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zm-40 728H184V184h656v656z"
+    ></path>
+  </svg>
+</template>
